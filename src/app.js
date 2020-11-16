624578cb8b1adef38dae29dfce557d238fc0f076
Keitysandrade <69424119+Keitysandrade@users.noreply.github.com>
2020-11-16 10:35:01 -0300

    Atualizando

--- src/app.js
+++ src/app.js
@@ -1,14 +1,30 @@
 const express = require("express")
 const bodyParser = require("body-parser")
+const mongoose = require("mongoose")
 
 const app = express()
+
+
+//String de conexão
+mongoose.connect("mongodb://localhost:27017/cursos", { 
+  useNewUrlParser: true, 
+  useUnifiedTopology: true 
+});
+
+//Conexão com o mongo
+let db = mongoose.connection;
+
+// Erro ou sucesso na conexão
+db.on("error", console.log.bind(console, "connection error:"))
+db.once("open", function (){
+  console.log("conexão foi feita com sucesso.")
+})
 
 //rotas
 const cursos = require("./routes/cursosRoute")
 
 //configurar body parser
 app.use(bodyParser.json());
-// app.use(express.json()); - Podemos usar a propria função de parser de json do express, sem a necessidade de instalar o body parser 
 
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*")
